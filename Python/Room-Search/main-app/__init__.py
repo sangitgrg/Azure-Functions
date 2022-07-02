181495a4ec4d55c7991b4ccb019833f6c858d7c3
Sangit <tamu.grgsangit@gmail.com>
2022-07-02 13:49:00 +0900

    added azure function for python

---
+++ Python/Room-Search/main-app/__init__.py
@@ -0,0 +1,3 @@
+import check_room
+
+check_room.initialize_process();
---
+++ Python/Room-Search/main-app/check_room.py
@@ -0,0 +1,51 @@
+import requests
+import configparser
+
+from mail_sender import SendMail
+
+
+def initialize_process():
+    url = ''
+    headers = {'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+
+    config = configparser.ConfigParser()
+    res = config.read('./main-app/config.ini')
+    print(config.sections())
+    danchi_list = config['App']['lists']
+    email_message = ''
+
+    xx = danchi_list.split('\n')
+    for danchi in xx:
+        danchi = danchi.strip()
+        danchi_info = danchi.split(':')
+        payload = {
+            "rent_low": "",
+            "rent_high": "",
+            "floorspace_low": "",
+            "floorspace_high": "",
+            "shisya": danchi_info[0],
+            "danchi": danchi_info[1],
+            "shikibetu": 0,
+            "newBukkenRoom": "",
+            "orderByField": 0,
+            "orderBySort": 0,
+            "pageIndex": 0,
+            "pageIndex": 0,
+            "sp": ""
+        }
+
+        res = requests.post(url, data=payload)
+        if(res.text != 'null'):
+            first_val = res.json()
+            avail_count = first_val[0]["allCount"]
+            if(avail_count == '1'):
+                email_message += f'{avail_count} rooms available at {danchi} at price {first_val[0]["rent"]} \n'
+                #print(f'{avail_count} rooms available at {danchi} at price {first_val[0]["rent"]}')
+            else:
+                email_message += f'{avail_count} rooms available at {danchi} \n'
+                #print(f'{avail_count} rooms available at {danchi}')
+        else:
+            email_message += f'no avialable at {danchi} \n'
+            #print(f'no avialable at {danchi}')
+
+    SendMail(email_message)
